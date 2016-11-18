5a9a809205be2155dd6b8a5ff9d78acce6133c6f
DarrenKimBat <uiucpass@gmail.com>
2016-11-19 01:29:29 +0900

    -레이아웃       -xml변경사항            mainactivity TitlteBar -> 타이틀바 구성추가            list_layout.xml -> gradlitview id를 listviewid로 설정
          -파일추가사항
               list_item.xml, list_layout 부분 추가
               BaseExpandableListAdapter.java 추가
    
          -버그리포트
               list_layout 렌더링 문제, 앱 실행 후 런타임 에러
    
          -기능추가
              탭 메모 기능을 위한 Expandable Listadapter 추가

---
+++ app/src/main/java/team2/apptive/tabmemo/BaseExpandableAdapter.java
@@ -0,0 +1,10 @@
+package team2.apptive.tabmemo;
+
+import android.widget.BaseExpandableListAdapter;
+
+/**
+ * Created by a0nes on 2016-11-19.
+ */
+
+public class BaseExpandableAdapter extends BaseExpandableListAdapter {
+}
